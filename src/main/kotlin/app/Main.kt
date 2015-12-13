64997fcca68251871c682a4893b32b38ddec9860
riz <riz@rizvn.com>
2015-12-13 16:12:07 +0000

    model example

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -1,15 +1,22 @@
 import app.Conf.view
 import spark.Route
 import spark.Spark.get
+import java.util.*
 
 fun main(args: Array<String>){
   //Routes
   get("/",        index)
-  get("/contact", contact)
   get("/about",   about)
+  get("/contact", contact)
 }
 
 //controllers
 val index   = Route { req, res -> view("index")  }
-val contact = Route { req, res -> view("contact")}
-val about   = Route { req, res -> view("about")  }
+
+val about   = Route { req, res -> view("about") }
+
+val contact = Route { req, res ->
+  val map = HashMap<String, Any>()
+  map["telNo"] = "0123456";
+  view("contact", model = map)
+}
